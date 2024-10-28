d25a9a1eba0ace6c65ae19b82474823b5b4a8196
Sonia <bhatiamandeep94@gmail.com>
2024-10-28 16:47:49 +0000

    NeetCode 3

--- Algorithms/src/main/java/NeetCode150/TwoSum.java
+++ Algorithms/src/main/java/NeetCode150/TwoSum.java
@@ -1,5 +1,32 @@
 package NeetCode150;
 
+import java.util.Arrays;
+
 public class TwoSum {
+
+public int[] twoSum(int[] nums, int target) {
+		
+        for(int i=0;i<nums.length;i++) {
+        	int rem = target - nums[i];
+        	System.out.println("Target "+target+", num "+nums[i]);
+        	System.out.println(rem);
+        		for(int j=i+1;j<nums.length;j++) {
+        			if(nums[j]==rem) {
+        				int[] arr = {i, j};
+        				return arr;
+        			}
+        		}
+        	
+        }
+        return null;
+    }
+
+	public static void main(String args[]) {
+		TwoSum t = new TwoSum();
+		int[] a = {3,2,3};
+		int[] result = t.twoSum(a, 6);
+		System.out.println(result[0]+" "+result[1]);
+	}
+
 
 }
