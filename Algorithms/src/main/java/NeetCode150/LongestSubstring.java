ea04d663fe3809e6f66e04bc4c86fb68244871df
Sonia <bhatiamandeep94@gmail.com>
2024-11-07 21:32:03 +0000

    Neet 150

--- Algorithms/src/main/java/NeetCode150/LongestSubstring.java
+++ Algorithms/src/main/java/NeetCode150/LongestSubstring.java
@@ -1,10 +1,32 @@
 package NeetCode150;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class LongestSubstring {
 
+	
+	public static int lengthOfLongestSubstring(String s) {
+		int len=0;
+		int l=0;
+		Set<Character> set = new HashSet<Character>();
+		for(int r= 0;r<s.length();r++) {
+			
+			while(set.contains(s.charAt(r))) {
+				set.remove(s.charAt(l));
+				l++;
+			}
+			
+			set.add(s.charAt(r));
+			len = Math.max(len, r-l+1);
+		}
+		
+		
+		return len;
+	}
+	
 	public static int withoutDuplicates(String s) {
 		int maxLen = 0;
 		int start = 0;
@@ -30,6 +52,6 @@ public class LongestSubstring {
 	}
 			
 	public static void main(String[] args) {
-		System.out.println(withoutDuplicates("abba"));
+		System.out.println(lengthOfLongestSubstring("abba"));
 	}
 }
