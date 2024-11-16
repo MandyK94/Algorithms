d8d0d2a30eec1b36f49c1e73218746ec485b4406
Sonia <bhatiamandeep94@gmail.com>
2024-11-16 15:20:25 +0000

    Neet 150

---
+++ Algorithms/src/main/java/NeetCode150/LinkedList.java
@@ -0,0 +1,63 @@
+package NeetCode150;
+
+public class LinkedList {
+
+	public class ListNode {
+		
+		public int val;
+		public ListNode next;
+		
+		public ListNode() {}
+
+		public ListNode(int val) {
+			this.val = val;
+		}
+		
+		public ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}	
+	}
+	
+	public static ListNode reverseList(ListNode head) {
+		
+        ListNode prev = null;
+        ListNode node = head;
+        while(node!=null) {
+        	ListNode temp = node.next;
+        	node.next=prev;
+        	prev = node;
+        	node = temp;
+        }
+        return prev;
+	}
+	
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		ListNode dummy = new ListNode(0);
+		ListNode node = dummy;
+		
+		while(list1!=null && list2!=null) {
+			if(list1.val<=list2.val) {
+				node.next = list1;
+				list1 = list1.next;
+			} else {
+				node.next = list2;
+				list2=list2.next;
+			}
+			node = node.next;
+		}
+		if(list1!=null) {
+			node.next=list1;
+		}
+		if(list2!=null) {
+			node.next=list2;
+		}
+		
+		
+		return dummy.next;
+	}
+	
+	public static void main(String[] args) {
+		LinkedList.reverseList(null);
+	}
+}
