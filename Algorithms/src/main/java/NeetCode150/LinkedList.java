2cfe3f963d6c973077511ee3e7704e77b432d270
Sonia <bhatiamandeep94@gmail.com>
2024-11-16 15:26:05 +0000

    Neet 150

--- Algorithms/src/main/java/NeetCode150/LinkedList.java
+++ Algorithms/src/main/java/NeetCode150/LinkedList.java
@@ -52,9 +52,22 @@ public class LinkedList {
 		if(list2!=null) {
 			node.next=list2;
 		}
-		
-		
 		return dummy.next;
+	}
+	
+	public boolean hasCycle(ListNode head) {
+		
+		ListNode ptr1=head;
+		ListNode ptr2=head;
+		
+		while(ptr2!=null && ptr2.next!=null) {
+			ptr1 = ptr1.next;
+			ptr2 = ptr2.next.next;
+			if(ptr1==ptr2) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public static void main(String[] args) {
