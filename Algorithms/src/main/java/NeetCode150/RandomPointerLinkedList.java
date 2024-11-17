a92e0b30b7f4f08142529769cdb28e034b2c8d12
Sonia <bhatiamandeep94@gmail.com>
2024-11-17 15:31:26 +0000

    Neet 150

---
+++ Algorithms/src/main/java/NeetCode150/RandomPointerLinkedList.java
@@ -0,0 +1,60 @@
+package NeetCode150;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RandomPointerLinkedList {
+
+	private class Node {
+	    int val;
+	    Node next;
+	    Node random;
+
+	    public Node(int val) {
+	        this.val = val;
+	        this.next = null;
+	        this.random = null;
+	    }
+	}
+	
+	public Node copyRandomList(Node head) {
+        
+		
+		if(head==null) {
+			return null;
+		}
+		
+		// Make a copy of nodes with val and insert them after original nodes
+		Node l1 = head;
+		while(l1!=null) {
+			Node l2= new Node(l1.val);
+			l2.next = l1.next;
+			l1.next = l2;
+			l1=l2.next;
+		}
+		
+		// copy random pointers
+		l1 = head;
+		while(l1!=null) {
+			if(l1.random!=null) {
+				l1.next.random = l1.random.next;
+			}
+			l1=l1.next.next;
+		}
+		
+		// remove the original nodes
+		Node newHead = head.next;
+		l1=head;
+		while(l1!=null) {
+			Node l2 = l1.next;
+			l1.next = l2.next;
+			if(l2.next!=null) {
+				l2.next = l2.next.next;
+			}
+			l1 = l1.next;
+		}
+    
+        return newHead;
+	}
+	
+}
