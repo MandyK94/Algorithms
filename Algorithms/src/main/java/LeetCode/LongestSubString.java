6f7e62313a144c18ed33a7a137578c6f0324d868
Sonia <bhatiamandeep94@gmail.com>
2025-04-01 17:53:34 +0100

    LeetCode

--- Algorithms/src/main/java/LeetCode/LongestSubString.java
+++ Algorithms/src/main/java/LeetCode/LongestSubString.java
@@ -1,13 +1,50 @@
 package LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubString {
 	
 	public int lengthOfLongestSubstring(String s) {
-		int count = 0;
+		if(s.length()==0) {
+			return 0;
+		}
+		int count = 1;
 		
+		int lPtr = 0;
+		int rPtr = 1;
 		
+		int currCount = 0;
+		
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		
+		map.put(s.charAt(0), 0);
+		
+		while(lPtr<s.length() && rPtr<s.length()& lPtr<=rPtr) {
+			if(map.containsKey(s.charAt(rPtr))) {
+				int nextIndex = map.get(s.charAt(rPtr));
+				while(lPtr<=nextIndex) {
+					map.remove(s.charAt(lPtr));
+					lPtr++;
+				}
+				lPtr = nextIndex+1;
+				map.put(s.charAt(rPtr), rPtr);
+				
+			} else {
+				map.put(s.charAt(rPtr), rPtr);
+				
+			}
+			rPtr++;
+			currCount = rPtr - lPtr;
+			count= Math.max(count, currCount);
+		}
 		
 		return count;
 	}
 
+	
+	public static void main(String[] args) {
+		LongestSubString l = new LongestSubString();
+		System.out.println(l.lengthOfLongestSubstring("abbbab"));
+	}
 }
